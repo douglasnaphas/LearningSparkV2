ff266bcc902d32ea8341efeccc324e4affa39bda
Douglas Naphas <dnaphas@healthverity.com>
2026-02-08 15:18:15 -0500

    Add the rest of example 2-1

--- doug/chapter2/py/src/mnmcount.py
+++ doug/chapter2/py/src/mnmcount.py
@@ -44,3 +44,24 @@ if __name__ == "__main__":
     # query to be executed.
     count_mnm_df.show(n=60, truncate=False)
     print("Total Rows = %d" % (count_mnm_df.count()))
+    # While the above code aggregated and counted for all
+    # the states, what if we just want to see the data for
+    # a single state, e.g., CA?
+    # 1. Select from all rows in the DataFrame
+    # 2. Filter only CA state
+    # 3. groupBy() State and Color as we did above
+    # 4. Aggregate the counts for each color
+    # 5. orderBy() in descending order
+    # Find the aggregate count for California by filtering
+    ca_count_mnm_df = (mnm_df
+        .select("State", "Color", "Count")
+        .where(mnm_df.State == "CA")
+        .groupBy("State", "Color")
+        .agg(count("Count").alias("Total"))
+        .orderBy("Total", ascending=False))
+    # Show the resulting aggregation for California.
+    # As above, show() is an action that will trigger the execution of the
+    # entire computation.
+    ca_count_mnm_df.show(n=10, truncate=False)
+    # Stop the SparkSession
+    spark.stop()
